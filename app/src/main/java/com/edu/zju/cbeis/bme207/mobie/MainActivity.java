fd9c27e32eef4c720aeff56d146aa7c826fa43c8
xfz329 <495390131@qq.com>
2022-05-17 21:01:01 +0800

    adjust menu

--- app/src/main/java/com/edu/zju/cbeis/bme207/mobie/MainActivity.java
+++ app/src/main/java/com/edu/zju/cbeis/bme207/mobie/MainActivity.java
@@ -1,17 +1,23 @@
 package com.edu.zju.cbeis.bme207.mobie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     @Override
@@ -22,11 +28,35 @@ public class MainActivity extends AppCompatActivity {
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(MainActivity.this,item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+//                String s;
+//                switch (item.getItemId()){
+//                    case R.id.nav_home:
+//                        s="home";
+//                        break;
+//                    case R.id.nav_view:
+//                        s = "view";
+//                        break;
+//                    case R.id.nav_gallery:
+//                        s = "gallery";
+//                        break;
+//                    default:
+//                        s="none";
+//                }
+//                Log.d("clicked",s );
+                drawerLayout.closeDrawer(navigationView);
+                return true;
+            }
+        });
+
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    public void onClick(View v) {
+
     }
 }
