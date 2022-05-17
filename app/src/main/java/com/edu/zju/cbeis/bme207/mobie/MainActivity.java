27e0a4b5e4543e24d6fcd754f6ae1e9a4261715e
xfz329 <495390131@qq.com>
2022-05-17 20:03:32 +0800

    drawerlayout

--- app/src/main/java/com/edu/zju/cbeis/bme207/mobie/MainActivity.java
+++ app/src/main/java/com/edu/zju/cbeis/bme207/mobie/MainActivity.java
@@ -1,14 +1,19 @@
 package com.edu.zju.cbeis.bme207.mobie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
